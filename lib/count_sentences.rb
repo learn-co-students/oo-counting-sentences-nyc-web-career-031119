96ef1f010643ed04955eff02f5be4a879f478449
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2019-03-12 18:07:57 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    parse = self.split()
+    punctuations = [".","?","!"]
+    count = 0
+    parse.each do |word|
+      if punctuations.include?(word[-1]) 
+        count+=1
+      end
+    end
+    count
   end
 end
