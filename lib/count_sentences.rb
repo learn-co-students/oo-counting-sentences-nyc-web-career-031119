1ddaa29c91d5091ff2f3e18413015610e96b89d6
david bernier <davidbernier91@gmail.com>
2019-03-12 23:10:02 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
+
+phrase = String.new("Hi, my name is Sophie.")
+
+puts phrase.sentence?
+puts phrase.question?
+puts phrase.exclamation?
+puts phrase.count_sentences
