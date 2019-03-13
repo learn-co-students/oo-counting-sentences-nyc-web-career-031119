41b33e714811cb993ddd5570423709c983f9d6a3
John Lee <jslee1992@gmail.com>
2019-03-13 14:03:21 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,19 +17,33 @@ class String
   def count_sentences
     count = 0
     self.split(' ').each do |word|
-      # binding.pry
-      if word.exclamation?
-        count += 1
-      elsif word.question?
-        count += 1
-      elsif word.sentence?
-        count += 1
-      else
-        count += 0
-      end
+
+      #Using Ternary Operator
+      word.exclamation? || word.question? || word.sentence? ? count +=1 : count +=0
+
+      # Using Case Statements
+      # case word[-1]
+      # when '?'
+      #     count += 1
+      #   when '!'
+      #     count += 1
+      #   when '.'
+      #     count += 1
+      # end
+
+      # Using Conditional Statements
+      # if word.exclamation?
+      #   count += 1
+      # elsif word.question?
+      #   count += 1
+      # elsif word.sentence?
+      #   count += 1
+      # else
+      #   count += 0
+      # end
     end
     count
   end
 end
 
-"asdf asd d d fa sdf".count_sentences
+"asdf asd.. d d fa!? sd.".count_sentences
