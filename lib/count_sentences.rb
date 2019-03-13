2c64ae79e8c31b41f92175c48df60d350f992468
github username <github email address>
2019-03-13 17:18:40 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
+#uses end_with method
 
 class String
 
   def sentence?
-    
-  end
+    self.end_with?('.')#returns true if ends in (.)
+  end                  #false if not
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sentence = []
+    sentence = self.split(/[.!?]/)
+    sentence.delete_if {|sentence| sentence.empty?}
+    sentence.count
   end
 end
