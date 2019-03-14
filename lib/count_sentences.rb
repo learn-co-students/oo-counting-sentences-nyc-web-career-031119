784797ecefee58c69daec0517c45a251a1de4ee8
authentic-actor-8801 <xaviernational@aol.com>
2019-03-14 02:15:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = []
+    self.split.each do |variable|
+      if variable.include?(".") || variable.include?("?") || variable.include?("!")
+        array << variable
+      end
+    end
+    array.count
   end
 end
